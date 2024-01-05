9c65588cd7e5fc334810fe47444d6d5d530173ff
Alexandre Sakata <alexandre1sakata@gmail.com>
2024-01-04 22:24:48 -0300

    new structure to client

---
+++ SinacorTestDev.Client/src/app/models/UserTask.ts
@@ -0,0 +1,8 @@
+export interface UserTask{
+    Id: number;
+    Name: string;
+    Description: string;
+    Status: string;
+    CreatedDate: Date;
+    ModifiedDate: Date;
+}
---
+++ SinacorTestDev.Client/src/app/services/user-task.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserTask } from '../models/UserTask';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserTaskService {
+
+  readonly apiUrl = `${environment.apiUrl}/UserTask`;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllTasks(): Observable<UserTask[]> {
+    return this.httpClient.get<UserTask[]>(this.apiUrl)
+  }
+  getTaskByName(id: number): Observable<UserTask> {
+    return this.httpClient.get<UserTask>(`${this.apiUrl}/${id}`);
+  }
+  createTask(userTask: UserTask): Observable<UserTask> {
+    return this.httpClient.post<UserTask>(this.apiUrl, JSON.stringify(userTask), this.httpOptions);
+  }
+  updateTask(id: number, userTask: UserTask): Observable<UserTask> {
+    return this.httpClient.put<UserTask>(`${this.apiUrl}/${id}`, JSON.stringify(userTask), this.httpOptions);
+  }
+  deleteTask(id: number): Observable<any> {
+    return this.httpClient.delete<UserTask>(`${this.apiUrl}/${id}`);
+  }
+}
